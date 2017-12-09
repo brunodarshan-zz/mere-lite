ad6c2ef405a8d585b45807b73acc5fa531b1f3fb
brunodarshan <brunodarshan@gmail.com>
2017-12-09 12:28:47 -0200

    change functions.php

--- functions.php
+++ functions.php
@@ -4,8 +4,20 @@ function mere_lite_scripts(){
     wp_enqueue_style("bootstrap", get_template_directory_uri()."/vendor/bootstrap/dist/css/bootstrap.min.css");
 
     // Adicionando scripts
-    wp_enqueue_script("boostrap-js", get_template_directory_uri()."/vendor/bootstrap/dist/js/bootstrap.min.js");
-    wp_enqueue_script("jquery", get_template_directory_uri()."/vendor/js/dist/jquery.min.js");
+    wp_enqueue_script(
+        $handle = "boostrap-js", 
+        $src =  get_template_directory_uri()."/vendor/bootstrap/dist/js/bootstrap.min.js",
+        $deeps = array(),
+        $ver = false,
+        $in_footer = true
+    );
+    wp_enqueue_script(
+        $handle = "jquery", 
+        $src = get_template_directory_uri()."/vendor/js/dist/jquery.min.js",
+        $deeps = array(),
+        $ver = false,
+        $in_footer = true
+    );
     
 }
 add_action("wp_enqueue_scripts", "mere_lite_scripts");
