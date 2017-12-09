6c1b97b9722d529b12aee35a556ff25d08c7bb28
brunodarshan <brunodarshan@gmail.com>
2017-12-09 13:55:28 -0200

    index.php

--- index.php
+++ index.php
@@ -1,28 +1,28 @@
 
-<?php get_header(); ?>
-<main class="postlist">
-    <div class="container">
-    <?php if (have_posts()): ?>
-        <div class="row">
-            <?php while (have_posts()) : the_post(); ?>
-                <div class="col">
-                '  <article class="card">
-                    <header class="card-block">
-                        <h2 class="card-title">
-                            <a href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a>
-                        </h2>
-                    </header>
+    <?php get_header(); ?>
+    <main class="postlist">
+        <div class="container">
+        <?php if (have_posts()): ?>
+            <div class="row">
+                <?php while (have_posts()) : the_post(); ?>
+                    <div class="col">
+                    '  <article class="card">
+                        <header class="card-block">
+                            <h2 class="card-title">
+                                <a href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a>
+                            </h2>
+                        </header>
 
-                    <div class="card-text">
-                        <?php the_content(); ?>
+                        <div class="card-text">
+                            <?php the_content(); ?>
+                        </div>
+                    </article>
                     </div>
-                 </article>
-                </div>
-            <?php endwhile; ?>
+                <?php endwhile; ?>
+            </div>
+        <?php endif; ?>'
         </div>
-    <?php endif; ?>'
-    </div>
-  
-</main>
-<?php get_footer(); ?>
+    
+    </main>
+    <?php get_footer(); ?>
 
